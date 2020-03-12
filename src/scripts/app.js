4b512ba32bd64d8c647fb501ef173b40e4659e07
Frederick Roegiers <frederick@rogerthat.be>
2020-03-12 09:58:45 +0100

    wip

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,16 +1,25 @@
 import '../styles/main.css';
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
-const helloWorld = () => {
-  // get our app container
-  const app = document.getElementById('app');
+mapboxgl.accessToken = 'pk.eyJ1IjoiZi1yb2dlcnMiLCJhIjoiY2p2MHFiem1iMTJhODN5bzRvenMwOWc0NSJ9.hAcN6R50XcaUwh0NI98Ifw';
+var map = new mapboxgl.Map({
+  container: 'app',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
 
-  // create a new DOM element
-  const element = document.createElement('div');
-  element.innerHTML = 'Hello World!';
 
-  // add to our app container
-  app.appendChild(element);
-};
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const long = position.coords.longitude;
+    const lat = position.coords.latitude;
 
-// start hellWorld app
-helloWorld();
+    const marker = document.createElement('div');
+    marker.className = 'marker';
+
+    new mapboxgl.Marker(marker)
+      .setLngLat([long, lat])
+      .addTo(map);
+  });
+} else {
+  console.log('Je browser is waarschijnlijk te oud');
+}
