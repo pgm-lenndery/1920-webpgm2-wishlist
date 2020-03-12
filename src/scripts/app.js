e82d5aeee49e3bbc88db96626bc93e94248c9faa
Frederick Roegiers <frederick@rogerthat.be>
2020-03-12 11:40:16 +0100

    wip

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,14 @@
 import '../styles/main.css';
+import * as CONFIG from './config';
 var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
+const OPENCAGE_API = `https://api.opencagedata.com/geocode/v1/json?pretty=1&key=${CONFIG.OPENCAGEAPIKEY}&q=`;
 
-const MAPBOX_TOKEN = 'pk.eyJ1IjoiZi1yb2dlcnMiLCJhIjoiY2p2MHFiem1iMTJhODN5bzRvenMwOWc0NSJ9.hAcN6R50XcaUwh0NI98Ifw';
 const MAPBOX_CONFIG = {
   container: 'app',
-  style: 'mapbox://styles/mapbox/streets-v11'
+  center: [3.7174243,51.0543422],
+  style: 'mapbox://styles/mapbox/streets-v11',
+  zoom: 10
 };
 
 function getCurrentPosition(options = {}) {
@@ -17,16 +20,21 @@ function getCurrentPosition(options = {}) {
 const app = {
   init() {
     // init mapbox
-    mapboxgl.accessToken = MAPBOX_TOKEN;
+    mapboxgl.accessToken = CONFIG.MAPBOX_TOKEN;
     this._map = new mapboxgl.Map(MAPBOX_CONFIG);
 
     // cache elements
     this.cacheDOMElements();
+    this.cacheDOMEvents();
 
     // get current location
     this.getCurrentLocation()
       .then(coords => {
+        // add first marker
         this.addMarker(coords);
+        this._map.flyTo({
+          center: coords
+        })
       });
   },
   async getCurrentLocation() {
@@ -35,11 +43,22 @@ const app = {
           const { latitude, longitude } = coords;
 
           return([longitude, latitude]);
-          // Handle coordinates
       } catch (error) {
-          // Handle error
           console.error(error);
       }
+  },
+  async fetchLocationCoordinates(address) {
+    const fetch_url = OPENCAGE_API + encodeURI(address);
+    console.log(fetch_url);
+    const response = await fetch(fetch_url)
+    const data = await response.json();
+    if(data.status.code == 200 && data.results.length > 0) {
+      const {lat, lng} = data.results[0].geometry;
+      return([lng, lat]);
+    }
+    else {
+      alert('Adres niet gevonden');
+    }
   },
   addMarker(coordinates) {
     const marker = document.createElement('div');
@@ -50,15 +69,21 @@ const app = {
       .addTo(this._map);
   },
   cacheDOMElements() {
-    this.$app = document.querySelector('app');
+    this.$app = document.querySelector('#app');
+    this.$inputAddress = document.querySelector('#address');
+    this.$buttonMagic = document.querySelector('#magic');
+  },
+  cacheDOMEvents() {
+      this.$buttonMagic.addEventListener('click', (e) => {
+        e.preventDefault();
+        const address = this.$inputAddress.value;
+        this.fetchLocationCoordinates(address)
+          .then((coords) => {
+            this.addMarker(coords);
+          })
+        
+    });
   }
 };
 
 app.init();
-
-
-
-
-
-
-// https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=...';
