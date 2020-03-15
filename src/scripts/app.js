5bec9aa5c6baaf2f100bc26b0c5d683c07324edd
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-03-15 19:05:24 +0100

    Builded new package

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,22 +4,47 @@ import {AIRTABlE_KEY} from './config';
 const appID = 'app8rag1axCaXV6TD'
 const apiURI = `https://api.airtable.com/v0/${appID}/wishlist`;
 
-console.log(apiURI + `?api_key=${AIRTABlE_KEY}`);
+// console.log(apiURI + `?api_key=${AIRTABlE_KEY}`);
 
 const table = document.querySelector('[data-label="wishlistItems"] .table');
 const contextMenu = document.querySelector('[data-label="contextMenu"]');
 
+let fieldMode = 'add'; // or edit
+
 const wishList = {
   initialize() {
+    this.addItemButton = document.querySelector('#addItem_magic');
+    
+    this.readyState();
     this.generateContextMenu();
     this.getAirtableData();
     this.selectedRecordID = '';
     
-    const addItemButton = document.querySelector('#addItem_magic');
-    addItemButton.addEventListener('click', () => {
-      this.addRecord();
-      this.addNewRecordToExistingList(this.getInputValues());
+    this.addItemButton.addEventListener('click', () => {
+      if (fieldMode == 'edit') {
+        this.updateRecord(this.selectedRecordID);
+        fieldMode == 'add';
+      } else {
+        this.addRecord();
+        // this.addNewRecordToExistingList(this.getInputValues());
+      }
+      // this.addRecord();
+      // this.addNewRecordToExistingList(this.getInputValues());
     })
+  },
+  
+  readyState() {
+    table.innerHTML = `
+      <div class="table-head">
+        <div class="col-3 th">Titel</div>
+        <div class="col-1 th">Prijs</div>
+        <div class="col-5 th">Omschrijving</div>
+        <div class="col-3 th">Link</div>
+      </div>
+    `;
+    fieldMode = 'add';
+    this.addItemButton.innerHTML = 'Item toevoegen';
+    this.selectedRecordID = '';
   },
   
   getInputValues() {
@@ -27,48 +52,10 @@ const wishList = {
       title: document.querySelector('#addItem_title').value,
       price: document.querySelector('#addItem_price').value,
       amount: document.querySelector('#addItem_amount').value,
-      description: document.querySelector('#addItem_amount').value,
+      description: document.querySelector('#addItem_description').value,
       url: document.querySelector('#addItem_url').value,
       imgUrl: document.querySelector('#addItem_img_url').value,
     }
-  },
-  
-  getAirtableData() {
-    fetch(apiURI, {
-      method: 'get',
-      headers: new Headers({
-        'Authorization': `Bearer ${AIRTABlE_KEY}`,
-        'Content-Type': 'application/json'
-      })
-    })
-    .then(response => response.json())
-    .then (data => {
-      this.generateWishlist(data);
-    });
-  },
-  
-  generateWishlist(data) {
-    console.log(data);
-    data.records.map(record => {
-      const itemData = record.fields;
-      
-      const itemUrlDomain = itemData.url.split('/')[2].replace('www.', '');
-      const div = document.createElement('div');
-      div.classList.add('tr', 'mb-3');
-      div.setAttribute('data-itemid', record.id);
-      
-      div.innerHTML = `
-        <div class="col-3 td">
-          <a href="${itemData.url}" target="_blank">${itemData.title}</a>
-        </div>
-        <div class="col-1 td">€${itemData.price.toString().replace('.',',')}</div>
-        <div class="col-5 td">${itemData.description}</div>
-        <div class="col-3 td">
-          <a href="${itemData.url}" target="_blank">Bekijk op ${itemUrlDomain}</a>
-        </div>
-      `
-      table.appendChild(div);
-    })
   },
   
   generateContextMenu() {
@@ -88,8 +75,8 @@ const wishList = {
     menuItem2.innerHTML = 'edit item';
     menuItem2.className = 'contextMenu-item';
     menuItem2.addEventListener('click', () => {
-      console.log('item2 click');
-      alert('lel da werkt nog nietn mut');
+      fieldMode = 'edit';
+      this.loadDataIntoInputFielfs(this.selectedRecordID);
       this.generateContextMenu();
     });
     contextMenu.appendChild(menuItem2);
@@ -116,9 +103,44 @@ const wishList = {
     })
   },
   
+  getAirtableData() {
+    fetch(apiURI, {
+      method: 'get',
+      headers: new Headers({
+        'Authorization': `Bearer ${AIRTABlE_KEY}`,
+        'Content-Type': 'application/json'
+      })
+    })
+    .then(response => response.json())
+    .then (data => {
+      this.generateWishlist(data);
+    });
+  },
+  
+  generateWishlist(data) {
+    data.records.map(record => {
+      const itemData = record.fields;
+      
+      const itemUrlDomain = itemData.url.split('/')[2].replace('www.', '');
+      const div = document.createElement('div');
+      div.classList.add('tr', 'mb-3');
+      div.setAttribute('data-itemid', record.id);
+      
+      div.innerHTML = `
+        <div class="col-3 td">
+          <a href="${itemData.url}" target="_blank">${itemData.title}</a>
+        </div>
+        <div class="col-1 td">€${itemData.price.toString().replace('.',',')}</div>
+        <div class="col-5 td">${itemData.description}</div>
+        <div class="col-3 td">
+          <a href="${itemData.url}" target="_blank">Bekijk op ${itemUrlDomain}</a>
+        </div>
+      `
+      table.appendChild(div);
+    })
+  },
+  
   addNewRecordToExistingList(input) {
-    console.log(input);
-    
     const itemUrlDomain = input.url.split('/')[2].replace('www.', '');
     const div = document.createElement('div');
     div.classList.add('tr', 'mb-3');
@@ -137,9 +159,60 @@ const wishList = {
     table.appendChild(div);
   },
   
+  loadDataIntoInputFielfs(inputID) {
+    fetch(`${apiURI}/${inputID}`, {
+      method: 'GET',
+      headers: new Headers({
+        'Authorization': `Bearer ${AIRTABlE_KEY}`,
+        'Content-Type': 'application/json'
+      })
+    })
+    .then(respone => respone.json())
+    .then(data => {
+      const itemData = data.fields
+      document.querySelector('#addItem_title').value = itemData.title;
+      document.querySelector('#addItem_price').value = itemData.price;
+      document.querySelector('#addItem_amount').value = itemData.amount;
+      document.querySelector('#addItem_description').value = itemData.description;
+      document.querySelector('#addItem_url').value = itemData.url;
+      document.querySelector('#addItem_img_url').value = itemData.img[0].url;
+    });
+    this.addItemButton.innerHTML = 'Item updaten';
+  },
+  
+  updateRecord(inputID) {
+    const input = this.getInputValues();
+    
+    fetch(`${apiURI}/${inputID}`, {
+      method: 'put',
+      headers: new Headers({
+        'Authorization': `Bearer ${AIRTABlE_KEY}`,
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({
+        "fields": {
+          "price": parseFloat(input.price),
+          "title": input.title,
+          "amount": parseFloat(input.amount),
+          "description": input.description,
+          "url": input.url,
+          "img": [
+            {
+              "url": input.imgUrl
+            }
+          ]
+        }
+      })
+    })
+    .then(response => response.json())
+    .then (data => {
+      this.readyState();
+      this.getAirtableData();
+    })
+  },
+  
   addRecord() {
     const input = this.getInputValues();
-    console.log(this.getInputValues())
     
     fetch(apiURI, {
       method: 'post',
@@ -166,7 +239,8 @@ const wishList = {
     })
     .then(response => response.json())
     .then (data => {
-      console.log(data);
+      this.readyState();
+      this.getAirtableData();
     })
   },
   
